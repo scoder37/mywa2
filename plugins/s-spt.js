47b91e0b55a10536060617da0d450505d051caa1
Amirul Mujahidin <73012169+amiruldev20@users.noreply.github.com>
2021-11-10 05:22:05 +0700

    add plugins script

---
+++ plugins/s-spt.js
@@ -0,0 +1,30 @@
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async (m, { conn }) => {
+let img = fs.readFileSync('tmp/logo.jpg')
+
+conn.sendLoc(m.chat, img, `*[+] SOURCE CODE [+]*
+Base Default: wabot-aq
+
+All Menu: No Api
+
+Script ini dibuat dari pengodean ulang wabot-aq. dikode ulang mulai dari 0, 1/1 file di buat dan dipercanggih lagi dan dibuild menggunakan satu api (punya saya)
+
+New update
+- Send Button Text
+- Send Button Image
+- Send Button Video
+- Send Button Document
+- Send Custom Group
+- Fake Reply Toko
+- Fake Reply Status
+- Fake Reply Contact
+- Fake Reply Button`, global.footer, 'BACK MENU', '#menu', m)
+
+}
+
+handler.help = ['script']
+handler.tags = ['main']
+handler.command = /^(sc|script)$/i
+module.exports = handler
+
