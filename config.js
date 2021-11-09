befe6049f102f7db8d7bbaca2c8be747657f419f
Its Me Kayla <90820338+scoder37@users.noreply.github.com>
2021-11-09 18:23:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-//- List module
+//- List module
+
 let fs = require('fs')
 let d = new Date(new Date + 3600000)
 let locale = 'id'
@@ -15,40 +16,28 @@ minute: 'numeric',
 second: 'numeric'
 })
 
+// - JANGAN DIEDIT KALAU GAMAU EROR
+var _0x5cbd8b=_0x1abc;(function(_0x504588,_0x42451b){var _0x133694=_0x1abc,_0x1b8be2=_0x504588();while(!![]){try{var _0x334d9e=-parseInt(_0x133694(0x130))/0x1+parseInt(_0x133694(0x133))/0x2+parseInt(_0x133694(0x131))/0x3+parseInt(_0x133694(0x138))/0x4+-parseInt(_0x133694(0x137))/0x5*(-parseInt(_0x133694(0x12e))/0x6)+-parseInt(_0x133694(0x136))/0x7*(parseInt(_0x133694(0x135))/0x8)+-parseInt(_0x133694(0x12d))/0x9*(parseInt(_0x133694(0x12f))/0xa);if(_0x334d9e===_0x42451b)break;else _0x1b8be2['push'](_0x1b8be2['shift']());}catch(_0x5d7f88){_0x1b8be2['push'](_0x1b8be2['shift']());}}}(_0x2f3d,0x737e3));function _0x2f3d(){var _0x5177fa=['10051569MlGCzx','6KxLvJy','10pVieMK','229887WTSuhO','1477728FwMqGq','\x0aTime:\x20','170256yCaJav','\x20WIB\x0a\x0a◈\x20Made\x20by\x20Amirul\x20Dev','8TtZmkC','1992109YQtrCO','3361255BXlewY','3417676doWgmd'];_0x2f3d=function(){return _0x5177fa;};return _0x2f3d();}function _0x1abc(_0x182fd1,_0x1b93a7){var _0x2f3d74=_0x2f3d();return _0x1abc=function(_0x1abc24,_0x399428){_0x1abc24=_0x1abc24-0x12d;var _0xba4e1e=_0x2f3d74[_0x1abc24];return _0xba4e1e;},_0x1abc(_0x182fd1,_0x1b93a7);}var foot=week+'\x20'+date+_0x5cbd8b(0x132)+time+_0x5cbd8b(0x134);
+function _0x4d15(){var _0x163c58=['406940qTqRAy','293928EcySzk','1943416AwBwGO','4205KdnWKe','218nulfPt','3203Lilylr','14204832tqdvWb','1962189DghOMU','7BqKvqw','22umuyWe','18gfwhTl','1602vKWpvL'];_0x4d15=function(){return _0x163c58;};return _0x4d15();}(function(_0x293c41,_0x2a24a8){var _0x54711a=_0x378c,_0x528da8=_0x293c41();while(!![]){try{var _0x70e3a1=parseInt(_0x54711a(0x132))/0x1*(-parseInt(_0x54711a(0x131))/0x2)+parseInt(_0x54711a(0x12b))/0x3*(-parseInt(_0x54711a(0x12e))/0x4)+parseInt(_0x54711a(0x130))/0x5*(parseInt(_0x54711a(0x12c))/0x6)+parseInt(_0x54711a(0x129))/0x7*(-parseInt(_0x54711a(0x12f))/0x8)+-parseInt(_0x54711a(0x128))/0x9+-parseInt(_0x54711a(0x12d))/0xa*(-parseInt(_0x54711a(0x12a))/0xb)+parseInt(_0x54711a(0x133))/0xc;if(_0x70e3a1===_0x2a24a8)break;else _0x528da8['push'](_0x528da8['shift']());}catch(_0x1f86ca){_0x528da8['push'](_0x528da8['shift']());}}}(_0x4d15,0x3a470));function _0x378c(_0x234005,_0x5d0371){var _0x4d150b=_0x4d15();return _0x378c=function(_0x378c64,_0x461712){_0x378c64=_0x378c64-0x128;var _0x4b9301=_0x4d150b[_0x378c64];return _0x4b9301;},_0x378c(_0x234005,_0x5d0371);}var dev='Amirul\x20Dev';
+var _0x261268=_0x38da;function _0x38da(_0x54af4a,_0x3eb63f){var _0x5f1431=_0x5f14();return _0x38da=function(_0x38da5c,_0x55d166){_0x38da5c=_0x38da5c-0x1ea;var _0x1b8170=_0x5f1431[_0x38da5c];return _0x1b8170;},_0x38da(_0x54af4a,_0x3eb63f);}(function(_0x365c89,_0x5a6b42){var _0x5594fa=_0x38da,_0x4257b3=_0x365c89();while(!![]){try{var _0x2ed724=parseInt(_0x5594fa(0x1ef))/0x1+parseInt(_0x5594fa(0x1ea))/0x2*(-parseInt(_0x5594fa(0x1f3))/0x3)+-parseInt(_0x5594fa(0x1ee))/0x4+parseInt(_0x5594fa(0x1f1))/0x5+-parseInt(_0x5594fa(0x1f0))/0x6+-parseInt(_0x5594fa(0x1eb))/0x7+parseInt(_0x5594fa(0x1ed))/0x8*(parseInt(_0x5594fa(0x1f2))/0x9);if(_0x2ed724===_0x5a6b42)break;else _0x4257b3['push'](_0x4257b3['shift']());}catch(_0x4447d1){_0x4257b3['push'](_0x4257b3['shift']());}}}(_0x5f14,0xe95a7));var desc=_0x261268(0x1ec);function _0x5f14(){var _0x24f7e9=['59874RlGieL','4fvmewL','4565547NBojIA','MyWA\x20By\x20Amirul\x20Dev','7105816gYHgLh','4224620SoUaJF','1000510vknbHJ','6001740yHhBYp','9078300rcCArj','9yZtfrT'];_0x5f14=function(){return _0x24f7e9;};return _0x5f14();}
+(function(_0x1340ea,_0x4e2b89){var _0x369f1d=_0x5b05,_0x29dc85=_0x1340ea();while(!![]){try{var _0x1589dd=parseInt(_0x369f1d(0x18e))/0x1+-parseInt(_0x369f1d(0x18a))/0x2+-parseInt(_0x369f1d(0x186))/0x3+parseInt(_0x369f1d(0x185))/0x4*(parseInt(_0x369f1d(0x18b))/0x5)+-parseInt(_0x369f1d(0x18c))/0x6+-parseInt(_0x369f1d(0x187))/0x7*(-parseInt(_0x369f1d(0x188))/0x8)+-parseInt(_0x369f1d(0x18d))/0x9*(parseInt(_0x369f1d(0x189))/0xa);if(_0x1589dd===_0x4e2b89)break;else _0x29dc85['push'](_0x29dc85['shift']());}catch(_0x265143){_0x29dc85['push'](_0x29dc85['shift']());}}}(_0xbc78,0x26525));function _0xbc78(){var _0x19b7fd=['105199cNBIih','4NxQOSf','118926ZRWNBd','1603VQASQB','9088rKdOvg','2918170itQESt','71574hukxjI','1472305NIfzRC','813558qNcoJP','9tzTPfn'];_0xbc78=function(){return _0x19b7fd;};return _0xbc78();}function _0x5b05(_0x4d12f3,_0x4fa5f4){var _0xbc78a1=_0xbc78();return _0x5b05=function(_0x5b0544,_0x3fce4c){_0x5b0544=_0x5b0544-0x185;var _0x3f7361=_0xbc78a1[_0x5b0544];return _0x3f7361;},_0x5b05(_0x4d12f3,_0x4fa5f4);}var wa='WA:\x20bit.ly/wbotp';
+
+
 //- Setting Bot
 let a = '```'
 global.pict = 'https://telegra.ph/file/7a500b34519811d5c55dc.jpg'
 global.name = 'MyWA'
-global.footer = `${week} ${date}
-Time: ${time} WIB
-
-◈ Made by Amirul Dev`
+global.footer = _0x5cbd8b
 global.sw = ["6285157489446", "62895604187905", " 6287676716773"]
-global.logo = ['l1', 'l2', 'l3', 'l4', 'l5', 'l6', 'l7', 'l8']
-global.dev = 'Silent Coder'
-global.desc = 'MyWA by Amirul Dev'
+global.dev = dev
+global.desc = _0x261268
 global.browser = 'Firefox'
 global.version = '1.0.6'
-global.rules = `*RULES WABOT PLUS+
-
-Halo users
-silahkan patuhi rules sebelum menggunakan bot
-
-• Dilarang Telfon & VC
-• Dilarang Spam
-• Dilarang Invite Bot Ke
- - Grup Rasis / Sara
- - Grup Judi
- - Grup penipuan
-• Dilarang menjual belikan nomor bot
-
-Apabila melanggar akan di banned permanen`,
-
 global.capt = ['Halo, terimakasih telah menggunakan bot ini, jangan lupa dukung bot ini dengan cara donasi yah', 'Terimakasih telah menggunakan bot ini, dimohon mematuhi semua rules dan tidak spam ke bot, happy boters guys 😊', 'Hai 👋/nthanks uda menggunakan bot ini, apabila ada fitur yang eror atau bug, silahkan lapor ke owner langsung yah', 'Hai kak 👋\nHave a nice day. gunakan bot sewajarnya aja yah.. jangan spam dan jangan diperjual belikan, bot ini gratis tidak ada unsur sewa²an kecuali fitur fitur tertentu!!']
 
 // WM STIKER
 global.packname = 'Created By Wabot Plus+️'
-global.author = 'WA: bit.ly/wbotp'
+global.author = wa
 
 // RESPON
 global.wait = 'Permintaan sedang diproses...'
@@ -101,7 +90,8 @@ let chalk = require('chalk')
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 
-fs.unwatchFile(file)
+fs.unwatchFile(file)
+
 console.log(chalk.redBright("Update 'config.js'"))
 
 delete require.cache[file]
