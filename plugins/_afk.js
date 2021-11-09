4ac5a41f798babc7f7f54656fe76e317870abe96
Amirul Mujahidin <73012169+amiruldev20@users.noreply.github.com>
2021-11-10 06:15:04 +0700

    add respon afk

---
+++ plugins/_afk.js
@@ -0,0 +1,55 @@
+//- Made by Amirul Dev
+//- FB: fb.com/amiruldev.id - IG: @amirul.dev
+
+let handler = m => m
+handler.before = m => {
+let user = global.db.data.users[m.sender]
+
+if (user.afk > -1) {
+
+conn.sendBtn(m.chat, `Kamu berhenti AFK ${user.afkReason ? ' setelah ' + user.afkReason : ''} Selama ${clockString(new Date - user.afk)}`, global.footer, 'LIST MENU', '#menu', m)
+
+user.afk = -1
+
+user.afkReason = ''
+
+}
+
+let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
+
+for (let jid of jids) {
+
+let user = global.db.data.users[jid]
+
+if (!user) continue
+
+let afkTime = user.afk
+
+if (!afkTime || afkTime < 0) continue
+
+let reason = user.afkReason || ''
+
+conn.sendBtn(m.chat, `Jangan tag dia!
+Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
+Selama ${clockString(new Date - afkTime)}`, global.footer, 'LIST MENU', '#menu', m)
+
+}
+
+return true
+
+}
+
+module.exports = handler
+
+function clockString(ms) {
+
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+
+return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+
+}
+
